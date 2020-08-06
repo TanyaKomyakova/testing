1c2ec5ac23a6b80272c70ee9a1158354932bbdad
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-08-06 13:36:31 +0300

    add tests

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -0,0 +1,39 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.Argument;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
+public class MobileBankApiTestV3 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+        ;
+    }
+
+
+}
